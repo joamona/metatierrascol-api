c08c5e57fd1a8bcb1c68cdae6082e1e91fd6befa
joamona <joamona@cgf.upv.es>
2023-12-21 10:03:53 +0100

    Cambios en app/serializer.py

--- metatierrascol/appcore/serializers.py
+++ metatierrascol/appcore/serializers.py
@@ -29,10 +29,16 @@ class LoginViewWithKnoxSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField(style={'input_type': 'password'}, trim_whitespace=False)
 
-    def validate(self, attrs):
+    def validate_username(self,value):
+        #validate_propiedad permite hacer validaciones adicionales sobre los campos
+        if len(value) > 10:
+            raise serializers.ValidationError('Username mayor que 10')
+        return value#hay que devolver un valor
+    
+    def validate(self, attrs): #se ejecuta con serializer.is_valid(raise_exception=True)
+                    #e inicializa validated_data, con los datos validados, que es un dict
         username = attrs.get('username').lower()
         password = attrs.get('password')
-
         user = authenticate(request=self.context.get('request'), username=username,
                             password=password)
         if not user:
