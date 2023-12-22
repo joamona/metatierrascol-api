248bcff0738f3f0e71b48a412dcd1be771a1c78f
joamona <joamona@cgf.upv.es>
2023-12-22 09:18:18 +0100

    Mensaje

--- metatierrascol/core/models.py
+++ metatierrascol/core/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class AppSettings(models.Model):
-    gid = models.AutoField(primary_key=True)
+    gid = models.AutoField(primary_key=True, editable=False)
     parameter_name = models.CharField(unique=True, max_length=50, blank=True, null=True)
     parameter_value = models.CharField(max_length=200, blank=True, null=True)
     help_en = models.CharField(max_length=200, blank=True, null=True)
