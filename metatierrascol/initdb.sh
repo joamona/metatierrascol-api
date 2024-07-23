b88d1e60faba7f8ba1365afeeceded9c40fd5818
joamona <joamona@cgf.upv.es>
2024-07-23 15:14:16 +0200

    Fixed script bug

--- metatierrascol/initdb.sh
+++ metatierrascol/initdb.sh
@@ -20,7 +20,7 @@ python manage.py shell < script/014_create_groups_emails.py
 python manage.py shell < script/015_create_email_settings.py
 python manage.py shell < script/016_create_email_settings_otros.py
 #python manage.py shell < script/017_update_django_sites.py
-python manage.py shell < script/script/018_create_table_passwordreset.py
+python manage.py shell < script/018_create_table_passwordreset.py
 
 
 
