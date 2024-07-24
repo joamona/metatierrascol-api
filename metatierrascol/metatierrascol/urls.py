9d2b94d7c3b4ce294bea7de03862cbe0d1033748
joamona <joamona@cgf.upv.es>
2024-07-24 23:35:45 +0200

    m

--- metatierrascol/metatierrascol/urls.py
+++ metatierrascol/metatierrascol/urls.py
@@ -39,7 +39,7 @@ schema_view = get_schema_view(
 urlpatterns = [
 #    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 #    path("accounts/", include("django.contrib.auth.urls")),
-    path('admin2/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'), 
     path('swagger/', login_required(schema_view.with_ui('swagger', cache_timeout=0)), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
